29043013033770b1b5ec8cda60468fbd1820e469
denisahmetov <denis.akhmetov.dev@gmail.com>
2023-11-19 21:01:59 +0600

    src/config/default Settings class changed

--- config/default.py
+++ config/default.py
@@ -1,24 +1,20 @@
-from os import getenv
-from dotenv import load_dotenv
 from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
-dotenv_path = Path(ROOT_DIR, "config", ".env")
-
-load_dotenv(dotenv_path)
 
 
 class Settings(BaseSettings):
-    TOKEN: str
+    BOT_TOKEN: str
 
     @property
     def bot_token(self):
-        token = getenv("BOT_TOKEN")
-        return token
+        return f"{self.BOT_TOKEN}"
 
     model_config = SettingsConfigDict(env_file=Path(ROOT_DIR, "config", '.env'))
 
 
 settings = Settings()
+
+print(settings.bot_token)
